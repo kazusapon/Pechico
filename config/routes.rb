ea9e6fd7d81ae65cf8d511e3eae06c058b881898
kazusapon <flum1278pool@gmail.com>
2021-09-02 00:12:54 +0900

    ユーザ一覧画面を作成(途中)

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
   resources :sessions, only: [:new, :create, :destroy]
   resources :dashboard, only: [:index]
+  resources :users, only: [:index, :create, :update, :destroy]
 end
