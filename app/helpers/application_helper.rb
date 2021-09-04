4a49fe4fc3d73e72641ba5f381bef34907dd3946
kazusapon <flum1278pool@gmail.com>
2021-09-04 22:55:06 +0900

    ユーザの操作ボタンを条件によって生成するヘルパーを用意

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  # 編集ボタンの生成
+  def edit_button(url, method, is_ajax=false)
+    btn = ''
+    btn += link_to('編集', url, method: method.to_sym, remote: is_ajax, class: 'btn btn-secondary mr-2')
+    
+    return btn.html_safe
+  end
+
+  # 削除ボタンの生成
+  def delete_button(url)
+    btn = ''
+    btn += link_to('削除', url, method: :delete, class: 'btn btn-danger mr-2')
+    
+    return btn.html_safe
+  end
+
+  # 削除取消（復活）ボタン
+  def reborn_button(url)
+    btn = ''
+    btn += link_to('削除取消', url, method: :get, class: 'btn btn-success mr-2')
+    
+    return btn.html_safe
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
 module UsersHelper
+  include ApplicationHelper
+
+  def user_operate_button(user)
+    str = ''
+    if user.deleted?
+      str += reborn_button(root_path)
+      return str.html_safe
+    end
+
+    str += edit_button(edit_user_path(id: user.id), 'get', true)
+    str += delete_button(user_path(id: user.id))
+    return str.html_safe
+  end
 end
