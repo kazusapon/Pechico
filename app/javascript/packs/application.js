15b0999c525cf81796b41e94cabe87e2e6141bec
kazusapon <flum1278pool@gmail.com>
2021-09-18 22:45:46 +0900

    flatpicker設定記述

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,13 +10,32 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "flatpickr"
 
+require('flatpickr')
+const japan = require("flatpickr/dist/l10n/ja.js").default.ja;
+$(document).on("turbolinks:load", () => {
+  const date_config = {
+    locale: japan
+  }
+  $(".datepickr").flatpickr(date_config);
+  
+  const time_config = {
+    enableTime: true,
+    noCalendar: true,
+    dateFormat: "H:i",
+    time_24hr: true
+  }
+  $(".timepickr").flatpickr(time_config);
+})
+
+// stylesheets
+require("../stylesheets/application.scss")
 var jQuery = require('jquery')
 global.$ = global.jQuery = jQuery;
 window.$ = window.jQuery = jQuery;
 
 import '../stylesheets/application';
+
 
 Rails.start()
 Turbolinks.start()
