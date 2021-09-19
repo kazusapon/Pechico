0cc751e4f4d3b3560f731d18a35fc252fc85d889
kazusapon <flum1278pool@gmail.com>
2021-09-19 23:51:45 +0900

    インスタンスメソッドを追加

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,4 +1,11 @@
 class Inquiry < ApplicationRecord
+  belongs_to :system
+  belongs_to :user
+  belongs_to :inquiry_classification
+  belongs_to :inquirier_kind
+  belongs_to :approver, class_name: 'User'
+  belongs_to :inquiry_relation
+
   validates :inquiry_date, presence: true
   validates :start_time, presence: true
   validates :end_time, presence: true
@@ -13,6 +20,21 @@ class Inquiry < ApplicationRecord
   enum inquiry_method_ids: { telephone: 1, mail: 2, direct: 3, other: 4 }
 
   scope :without_deleted, -> { where(deleted_at: nil) }
+
+  def system_name
+    return system.name
+  end
+
+  def user_name
+    return user.name
+  end
+
+  def inquiry_datetime_text
+    date = self.inquiry_date.strftime('%Y-%m-%d')
+    time = self.start_time.strftime('%H:%M')
+
+    return date + time
+  end
 
   def self.complete_options
     return { '未完了' => is_completeds[:incomplete], '完了' => is_completeds[:complete], }
