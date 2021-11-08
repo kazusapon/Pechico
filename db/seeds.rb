a49cb7a2e1b6d8426ac097b8ba1d8f0f27bf8f79
kazusapon <flum1278pool@gmail.com>
2021-11-08 23:59:20 +0900

    シード修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,16 +17,18 @@ if User.count < 1
 end
 
 # 新着問合せ登録
-if UnregisterInquiry.count < 1
-  user = User.first
-  10.times do
-    UnregisterInquiry.create!(
-      user_id: user.id,
-      company_name: 'テスト工業株式会社',
-      telephone_number: '0852-22-3455',
-      inquiry_date: Time.zone.now.strftime('%Y-%m-%d'),
-      start_time: '10:20',
-      end_time: '13:00',
-    )
-  end
+UnregisterInquiry.destroy_all
+user = User.first
+10.times do
+  UnregisterInquiry.create!(
+    user_id: user.id,
+    company_name: 'テスト工業株式会社',
+    telephone_number: '0852-22-3455',
+    inquiry_date: Time.zone.now.strftime('%Y-%m-%d'),
+    start_time: '10:20',
+    end_time: '13:00',
+    inquirier_name: 'テスト太郎',
+    inquirer_kind_id: InquirierKind.first.id,
+    unknown_number_status: 10
+  )
 end
