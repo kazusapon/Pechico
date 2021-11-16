d5f19b86ecbf0efef4d67253cb7387b3ff8669e6
kazusapon <flum1278pool@gmail.com>
2021-11-17 07:11:04 +0900

    サマリ部分作成

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,17 @@
 class DashboardController < ApplicationController
   def index
+    inquiries = Inquiry.without_deleted
+    build_summary(inquiries)
+  end
+
+  private
+
+  def build_summary(inquiries)
+    @total_count = inquiries.size
+    @average_minutes = inquiries.average('end_time - start_time') / 60
+    @max_minutes = inquiries.maximum('end_time - start_time') / 60
+
+    system_count = inquiries.group(:system_id).count().max{|a, b| a <=> b}
+    @maximum_system_name = System.find(system_count.first).name
   end
 end
