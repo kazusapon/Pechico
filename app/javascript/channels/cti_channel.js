760eb1257781455d538ed47571fcfe16bed98d46
kazusapon <flum1278pool@gmail.com>
2021-12-30 00:01:50 +0900

    色を変更

--- app/javascript/channels/cti_channel.js
+++ app/javascript/channels/cti_channel.js
@@ -21,29 +21,30 @@ consumer.subscriptions.create("CtiChannel", {
   },
 
   _buildCtiModal(data) {
+    const bg = 'success';
     const html = `
       <button type="button" id="cti-modal-open-button" style="display: none;" data-bs-toggle="modal" data-bs-target="#cti-modal">ctiModalOPEN</button>
       <div class="modal fade" id="cti-modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-modal="true" role="dialog">
         <div class="modal-dialog modal-lg">
-          <div class="modal-content bg-danger">
+          <div class="modal-content bg-${bg}">
             <div class="modal-header">
               <h4 class="modal-title">着信があります</h4>
             </div>
             <div class="modal-body">
               <ul class="list-group list-group-flush fs-3">
-                <li class="list-group-item list-group-item-danger">
+                <li class="list-group-item list-group-item-${bg}">
                   <i class="fas fa-clock mr-3"></i> ${data.datetime}
                 </li>
-                <li class="list-group-item list-group-item-danger">
+                <li class="list-group-item list-group-item-${bg}">
                   <i class="fas fa-industry mr-3"></i> ${data.company_name}
                 </li>
-                <li class="list-group-item list-group-item-danger">
+                <li class="list-group-item list-group-item-${bg}">
                   <i class="fas fa-user-tie mr-3"></i> ${data.inquirier_name}
                 </li>
-                <li class="list-group-item list-group-item-danger">
+                <li class="list-group-item list-group-item-${bg}">
                   <i class="fas fa-phone mr-3"></i> ${data.telephone_number}
                 </li>
-                <li class="list-group-item list-group-item-danger">
+                <li class="list-group-item list-group-item-${bg}">
                   <i class="fas fa-user-friends mr-3"></i> ${data.inquirier_kind}
                 </li>
               </ul>
